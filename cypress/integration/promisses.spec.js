91e834aca4a9e00fe8bbcd2cb211eca1d2634e01
Wesley <podwesley@gmail.com>
2023-11-13 16:56:41 -0300

    promisses como usar

--- cypress/integration/promisses.spec.js
+++ cypress/integration/promisses.spec.js
@@ -2,16 +2,24 @@ it('teste promisses 1', () => {
 
 })
 
-const getSomeThing = callback => {
-    setTimeout(() => {
-        callback(12);
-    }, 1000)
+const getSomeThing = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(12);
+        }, 1000)
+    })
+
 }
 
 const system = () => {
     console.log('init')
-    getSomeThing(some => console.log(`something é: ${some}`));
-    console.log('end')
+    const prom = getSomeThing();
+    prom.then(some => {
+        console.log(`something é: ${some}`)
+        console.log('end')
+    })
+
+
 }
 
 system();
