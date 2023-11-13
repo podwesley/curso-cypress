e687e9ec294e63d576aacf3c54263f5090fee24d
Wesley <podwesley@gmail.com>
2023-11-13 17:23:57 -0300

    describe e it

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('agrupar testes' , () => {
+
+    describe('agrupar testes mais especificos' , () => {
+        it('teste 1 e', () => {
+
+        })
+    })
+
+    it('teste 1', () => {
+
+    })
+
+    it('teste 2', () => {
+
+    })
+
+})
