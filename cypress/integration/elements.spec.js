51d61396a120c3c2c7c45968e4fc44936e3972a8
Wesley <podwesley@gmail.com>
2023-11-14 15:17:10 -0300

    validacoes links elementos , reload.

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -2,12 +2,29 @@
 
 describe('Elementos básico', () => {
 
-    it.only('text', () => {
+    it('text', () => {
 
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
 
         cy.get('.facilAchar') //class element.
-        .should('have.text', 'Cuidado onde clica, muitas armadilhas...')
+            .should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
+
+    it.only('links', () => {
+
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+        cy.get('[href="#"]')
+            .click()
+            .should('have.not.text', 'Voltou!')
+            .get('#resultado')
+            .should('have.text', 'Voltou!')
+            .reload() // Carregar a pagina novamente
+            .get('#resultado')
+            .should('have.text', 'Status: Nao cadastrado')
+
+  
+    })
+
 
 })
