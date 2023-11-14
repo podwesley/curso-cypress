211f06f586923151f250e2baddf6377c57267c1f
Wesley <podwesley@gmail.com>
2023-11-14 14:22:10 -0300

    escrevendo fluente

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -3,12 +3,14 @@
 describe('Básico de cypress', () => {
 
     it('visitar uma nova pagina e fazer uma validação do título', () => {
+
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
 
-        //const title = cy.title()
-        //console.log(title)
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
 
-        cy.title().should('be.equal', 'Campo de Treinamento')
+        //imprimir o log no console, e escrever o log em um campo de texto. 
 
     })
 
