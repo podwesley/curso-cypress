82cf2d07428a2bf50cb59e0f79d2687032b66c55
Wesley <podwesley@gmail.com>
2023-11-14 17:18:06 -0300

    waits example 1

---
+++ cypress/integration/waits.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+
+describe('Waits ...', () => {
+
+    before(() => { //Vai rodar uma vez antes de todos os testes. 
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => { // vai executar antes de cada teste. 
+        cy.reload() // Carregar a pagina novamente
+    })
+
+    it('1 - Deve aguardar elemento disponível', () => {
+
+        cy.get('#novoCampo').should('not.exist')
+        .get('#buttonDelay')
+        .click()
+        .get('#novoCampo').type('funciona')
+
+    })
+})
