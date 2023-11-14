843fb108d5a50b1b2f90dd30be8733a25e4865ba
Wesley <podwesley@gmail.com>
2023-11-14 11:01:52 -0300

    fazendo validações gerais

--- cypress/integration/asserts.spec.js
+++ cypress/integration/asserts.spec.js
@@ -32,3 +32,31 @@ it('Truthy ', () => {
     expect(c).to.be.undefined;
 
 })
+
+it('Objects Equality', () => {
+
+    const obj = {
+        a: 1,
+        b: 2
+    };
+
+
+    expect(obj, 'Verificar se objetos são iguais').equal(obj);
+    expect(obj, 'Forma alternativa de verificar se "obj" é igual a "obj"').equals(obj);
+    expect(obj, 'Outra Forma alternativa de verificar se "obj" é igual a "obj"').eq(obj);
+
+    //expect(obj).to.be.equal({a:1, b: 2});
+    expect(obj, 'Verifica se "obj" é profundamente igual a {a: 1, b: 2}').to.be.deep.equal({ a: 1, b: 2 });
+    expect(obj, 'Outra forma de verificar se "obj" é igual a {a: 1, b: 2}').eql({ a: 1, b: 2 });
+
+    expect(obj, 'Verifica se "obj" inclui a propriedade "a" com o valor 1').include({ a: 1 });
+
+    expect(obj, 'Verifica se "obj" tem a propriedade "b"').to.have.property('b');
+
+    expect(obj, 'Verifica se "obj" tem a propriedade "b" com o valor 2').to.have.property('b', 2);
+
+    expect(obj, 'Verifica se "obj" não está vazio').to.not.be.empty;
+
+    expect({}, 'Verifica se o objeto vazio é realmente vazio').to.be.empty;
+
+})
