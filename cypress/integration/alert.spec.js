f8d202b6c09190883f7f19305b5867e82144d8e9
Wesley <podwesley@gmail.com>
2023-11-17 20:18:09 -0300

    desafio validar mensagens alerts, prompts.

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -83,4 +83,38 @@ describe('Alerts', () => {
             .click()
 
     })
+
+    it.only('validando mensagens', () => {
+
+        const stub = cy.stub().as('alerta')
+
+        cy.on('window:alert', stub)
+
+        cy.get('#formCadastrar')
+            .click()
+            .then(() => {
+                expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio')
+            })
+
+        cy.get('#formNome').type('Wesley')
+        cy.get('#formCadastrar')
+            .click()
+            .then(() => {
+                expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio')
+            })
+
+        cy.get('[data-cy=dataSobrenome]').type('Soares')
+        cy.get('#formCadastrar')
+            .click()
+            .then(() => {
+                expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio')
+            })
+
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado > :nth-child(1)')
+            .should('contain', 'Cadastrado!')
+
+
+    })
 })
