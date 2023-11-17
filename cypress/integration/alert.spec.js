a2051a1c2590d023863127fba81dc2d9f0688db9
Wesley <podwesley@gmail.com>
2023-11-17 18:53:41 -0300

    alert com stub

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -10,13 +10,26 @@ describe('Alerts', () => {
         // cy.reload() // Carregar a pagina novamente
     })
 
-    it.only('Alert 1', () => {
+    it('Alert 1', () => {
 
         cy.get('#alert')
             .click()
         cy.on('window:alert', mensagem => { //pega eventos que ocorrem na tela
             console.log(mensagem)
             expect(mensagem).to.be.equal('Alert Simples')
+        })
+
+    })
+
+
+    it.only('Alert com stub/mock', () => {
+
+        const mock = cy.stub().as('alerta')// o as é um alias chamado alerta
+
+        cy.on('window:alert', mock)
+        cy.get('#alert')
+            .click().then(() => {
+                expect(mock.getCall(0)).to.be.calledWith('Alert Simples')
         })
 
     })
