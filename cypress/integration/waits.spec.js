2dee2050f9ebca0f120bf896eb5583eb5f6a3820
Wesley <podwesley@gmail.com>
2023-11-17 15:09:51 -0300

    then vc shold

--- cypress/integration/waits.spec.js
+++ cypress/integration/waits.spec.js
@@ -92,5 +92,56 @@ describe('Waits ...', () => {
 
     })
 
+    it.only('then vs should', () => {
+
+        // o should fica sendo executado ao longo da espera e o then aguarda a requisição até que o get finalize.
+
+        // usando o shold
+        cy.get('#buttonListDOM')
+            .click()
+            .get('#lista li span')
+            .should('have.length', 1)
+        //.debug()
+
+        // then tratando com promessas 
+        cy.get('#buttonListDOM')
+            .get('#lista li span')
+            .then(elemento => {
+                console.log('then() 1 - elemento html: ', elemento)
+                expect(elemento, '').to.have.length(1)
+            })
+
+        // then tratando com promessas 
+        cy.get('#buttonListDOM')
+            .get('#lista li span')
+            .should(elemento => {
+                console.log('shoud() 1 - elemento html: ', elemento)
+                expect(elemento, '').to.have.length(1)
+            })
+
+
+        // then ele retorna alguma coisa alguma alteração só quem considera o return é o then
+        cy.get('#buttonListDOM')
+            .then(elemento => {
+                console.log('then() 2 - elemento html: ', elemento)
+                expect(elemento, '').to.have.length(1)
+                return 2
+            })
+            .and('eq', 2)
+            .and('not.have.id', 'buttonListDOM')
+
+
+
+        // should vai retornar sempre o elemento dentro da função e ignora qualquer retorno
+        cy.get('#buttonListDOM')
+            .should(elemento => {
+                console.log('then() 2 - elemento html: ', elemento)
+                expect(elemento, '').to.have.length(1)
+                return 2
+            })
+            .and('have.id', 'buttonListDOM')
+
+    })
+
 
 })
